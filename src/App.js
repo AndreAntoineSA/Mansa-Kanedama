7d267637fac989e7ddd4d4adf222d922f3ec899d
andre <andreantoine0016@gmail.com>
2021-08-12 22:14:15 +0200

    Setting up router, redux and styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Navbar, Home, Profile, Account } from "./components"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import GlobalStyle from "./globalStyles"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <GlobalStyle />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/account" exact component={Profile} />
+          <Route path="/account/:account_id" exact component={Account} />
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/globalStyles.js
@@ -0,0 +1,36 @@
+import styled, { createGlobalStyle, css } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin:0;
+    padding:0;
+    font-family:'Source Sans Pro', sans-serif;
+}
+`
+export const Container = styled.div`
+  z-index: 1;
+  width: 100%;
+  max-width: 1300px;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 50px;
+  padding-left: 50px;
+
+  @media screen and (max-width: 991px) {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+`
+
+export const sharedStyles = css`
+  background-color: #eee;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  margin: 10px 0 20px 0;
+  padding: 20px;
+  box-sizing: border-box;
+`
+
+export default GlobalStyle
